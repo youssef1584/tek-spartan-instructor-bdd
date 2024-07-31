1e3514ce2639f8756d28f69192ba282e987336f7
youssef1584 <youssefmoe1584@gmail.com>
2024-07-31 12:56:30 -0700

    make code maintainable

--- src/test/java/tek/bdd/utility/SeleniumUtility.java
+++ src/test/java/tek/bdd/utility/SeleniumUtility.java
@@ -3,6 +3,7 @@ package tek.bdd.utility;
 import com.google.common.eventbus.SubscriberExceptionContext;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import tek.bdd.base.BaseSetup;
@@ -18,16 +19,30 @@ import static java.sql.DriverManager.getDriver;
            return new WebDriverWait(getDriver(), Duration.ofSeconds(20));
        }
 
+       private WebElement waitForVisibility(By locator){
+           return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+
+       }
+
     // create a method to click on a given locator
 
        public void clickOnElement(By locator) {
           // WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
-           getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
+          getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
+
        }
 
        public void sendText (By locator, String value){
 
-           getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(value);
+           //getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(value);
+           waitForVisibility(locator).sendKeys(value);
+
+       }
+
+       public String getElementText(By locator){
+         return   waitForVisibility(locator).getText();
+
 
        }
 
